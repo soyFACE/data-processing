959a162e12c16cc4a27b0e904337a5ec4e067cc6
yuxifan2 <fanyuxi0706@gmail.com>
2019-11-07 11:44:16 -0600

    change data loading method

--- analysis_files/processing_trial.R
+++ analysis_files/processing_trial.R
@@ -1,6 +1,9 @@
 source("analysis_files/soyface_data_processing_functions.R")
 
 #############################Load data################################
+
+load("processed_r_data/sfdata.Rdata")
+
 valid_range <- read.csv("metadata/valid_ranges.csv"
                         ,stringsAsFactors = FALSE
                         ,colClasses = c('character'))
@@ -26,16 +29,17 @@ fumigation_type <- read.csv("metadata/fumigation_type.csv"
                             ,colClasses = 'character'
 )
 ######################################################################
-
-sfdata <- raw_sfdata_avg_to_dataframe("\\\\commons2.life.illinois.edu\\soyface_fumigation_data\\2019\\")
+## sfdata <- raw_sfdata_avg_to_dataframe("\\\\commons2.life.illinois.edu\\soyface_fumigation_data\\2019\\") ## Runing time 3.330841 mins
 
 sfdata <- sfdata[rowSums(is.na(sfdata)) != ncol(sfdata),] ##Delet empty row
 
-unconvertible_rows <- check_sfdata_types(sfdata,valid_range) ## Serch for unconvertible datapoints 
 
-out_of_range_rows <- check_sfdata_range(sfdata,valid_range) ## Search for out of range datapoints
+unconvertible_rows <- check_sfdata_types(sfdata,valid_range) ## Serch for unconvertible datapoints (Runing time: 18.89308 secs)
 
-sfdata_with_metadata <- add_sfdata_metadata(sfdata) ## Merge with metadata
 
+out_of_range_rows <- check_sfdata_range(sfdata,valid_range) ## Search for out of range datapoints  (Runing time: 52.14598 secs)
+
+
+sfdata_with_metadata <- add_sfdata_metadata(sfdata) ## Merge with metadata (Runing time: 25.51346 secs)
 
 
