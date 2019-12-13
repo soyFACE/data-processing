7479c87f445ca8700cb663d38598f0d232e39853
SoyFACE <soyface1114@gmail.com>
2019-12-13 12:07:27 -0600

    added a chart for the ambient monitor

--- analysis_files/processing_trial.R
+++ analysis_files/processing_trial.R
@@ -54,6 +54,8 @@ sfdata_6_fill_gaps <- fill_gaps(sfdata_5_in_valid_date_range)
 sfdata_6_fill_gaps$layer_2_setpoint <-  0
 out_of_range_rows <- check_sfdata_range(sfdata_6_fill_gaps,valid_range)
 
+out_of_range_rows <- out_of_range_rows[order(out_of_range_rows$Range_flag,out_of_range_rows$ring_id,out_of_range_rows$dt),]
+
 write.csv(out_of_range_rows, file = "out_of_range/out_of_range_all_variables.csv"
           ,row.names = FALSE)
 #create_groupby_csv(out_of_range_rows)
